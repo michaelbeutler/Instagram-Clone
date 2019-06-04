7365b45e93de0288cda5c4d91b870e8c8f93caab
MichaelBeutler <michael.beutler@gibmit.ch>
2019-06-04 16:41:11 +0200

    added json response

--- posts/views.py
+++ posts/views.py
@@ -14,174 +14,42 @@ class IndexView(TemplateView):
 @require_http_methods(["GET"])
 def get_posts(request):
     posts = Post.objects.all()
+    post_data = []
+    for post in posts:
+        comment_data = []
+        for comment in post.comments.all():
+            comment_data.append(
+                {
+                    "user": {
+                        "username": comment.account.username,
+                        "url": comment.account.slug,
+                        "avatar": comment.account.avatar.url
+                    },
+                    "comment": comment.text
+                }
+            )
+        post_data.append({
+            "user": {
+                "username": post.account.username,
+                "url": post.account.slug,
+                "avatar": post.account.avatar.url
+            },
+            "image": post.image.image.url,
+            "location": post.location.name,
+            "caption": post.caption,
+            "date": naturalday(post.date).upper(),
+            "comment": True,
+            "comments": comment_data,
+            "likes": [],
+            "liked": False       
+        })
+    
+
     data = {
         "code": 200,
         "description": "success",
         "data": {
-            "posts": [
-                {
-                    "user": {
-                        "username": posts[0].account.username,
-                        "url": posts[0].account.slug,
-                        "avatar": posts[0].account.avatar.url
-                    },
-                    "image": posts[0].image.image.url,
-                    "location": posts[0].location.name,
-                    "caption": posts[0].caption,
-                    "date": naturalday(posts[0].date).upper(),
-                    "comment": True,
-                    "comments": [
-                        {
-                            "user": {
-                                "username": posts[0].comments.all()[0].account.username,
-                                "url": posts[0].comments.all()[0].account.slug,
-                                "avatar": posts[0].comments.all()[0].account.avatar.url
-                            },
-                            "comment": posts[0].comments.all()[0].text
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "comment": "test"
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "comment": "test"
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "comment": "test"
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "comment": "test"
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "comment": "test"
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "comment": "test"
-                        }
-                    ],
-                    "likes": [
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "date": "2019/06/03"
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "date": "2019/06/03"
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "date": "2019/06/03"
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "date": "2019/06/03"
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "date": "2019/06/03"
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "date": "2019/06/03"
-                        },
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "date": "2019/06/03"
-                        }
-                    ],
-                    "liked": True
-                },
-                {
-                    "user": {
-                        "username": "michi.beutler",
-                        "url": "slug",
-                        "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                    },
-                    "image": "img/60450661_2376164995975286_6308187267627809876_n.jpg",
-                    "location": "Example Location1",
-                    "caption": "Love these sunsets#ktm🥰",
-                    "date": "VOR 30 SEKUNDEN",
-                    "comment": True,
-                    "comments": [
-                        {
-                            "user": {
-                                "username": "timon.74",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "comment": "sooo niceeee 😍"
-                        }
-                    ],
-                    "likes": [
-                        {
-                            "user": {
-                                "username": "michi.beutler",
-                                "url": "slug",
-                                "avatar": "img/42002584_995628447264876_5615867123332022272_n.jpg"
-                            },
-                            "date": "2019/06/03"
-                        }
-                    ],
-                    "liked": False
-                }
-            ]
+            "posts": post_data
         }
     }
     return JsonResponse(data)
